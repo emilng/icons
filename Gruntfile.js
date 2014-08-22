ec357a503cd9f032820602daf29b0234db361d4f
Emil Ng <github@alphabeticaldisorder.com>
2014-08-22 00:27:59 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,109 @@
+module.exports = function (grunt) {
+  'use strict';
+  // Project configuration
+  grunt.initConfig({
+    // Metadata
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+    // Task configuration
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/icons.js'],
+        dest: 'dist/icons.js'
+      }
+    },
+    browserify: {
+      client: {
+        src: 'src/**/*.js',
+        dest: 'dist/icons.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/icons.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        node: true,
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        eqnull: true,
+        browser: true,
+        globals: { jQuery: true },
+        boss: true
+      },
+      gruntfile: {
+        src: 'gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    cssUrlEmbed: {
+      encodeDirectly: {
+        options: {
+          baseDir: '.'
+        },
+        files: {
+          'dist/main.css': ['styles/main.css']
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: '.',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-css-url-embed');
+
+  // Default task
+  grunt.registerTask('default', ['jshint', 'browserify', 'uglify']);
+  grunt.registerTask('css', ['cssUrlEmbed']);
+};
+
---
+++ src/icons.js
@@ -0,0 +1,56 @@
+window.onload = function(event){
+  console.log('page loaded');
+  var canvas = document.getElementById('view_container');
+  var ctx = canvas.getContext('2d');
+  var height = 640;
+  var width = 640;
+  var shade;
+
+  // function shamelessly stolen from 
+  // http://stackoverflow.com/questions/7126384/expressing-utf-16-unicode-characters-in-javascript
+  function getUnicodeCharacter(cp) {
+
+      if (cp >= 0 && cp <= 0xD7FF || cp >= 0xE000 && cp <= 0xFFFF) {
+          return String.fromCharCode(cp);
+      } else if (cp >= 0x10000 && cp <= 0x10FFFF) {
+
+          // we substract 0x10000 from cp to get a 20-bits number
+          // in the range 0..0xFFFF
+          cp -= 0x10000;
+
+          // we add 0xD800 to the number formed by the first 10 bits
+          // to give the first byte
+          var first = ((0xffc00 & cp) >> 10) + 0xD800
+
+          // we add 0xDC00 to the number formed by the low 10 bits
+          // to give the second byte
+          var second = (0x3ff & cp) + 0xDC00;
+
+          return String.fromCharCode(first) + String.fromCharCode(second);
+      }
+  }
+
+  // var arrow = '\u2B06';
+  var arrow = '\uE767';
+  var down_arrow = '\uE764';
+  var drops = '\u1F4A6';
+
+  var drops = getUnicodeCharacter(0x1F4A6);
+  console.log(drops);
+  
+  var render = function() {
+    shade = Math.round(Math.random()*255);
+    ctx.fillStyle = "rgba(" +shade + "," + shade + "," + shade + "," + Math.random() + ")";
+    ctx.font = Math.ceil(Math.random()*300) + 'px "Entypo"';
+
+    var tx = Math.random()*width;
+    var ty = Math.random()*height;
+    ctx.fillText(drops, tx, ty);
+    
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+};
+
+
